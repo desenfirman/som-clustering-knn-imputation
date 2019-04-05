4d762595f9178689daa3b008c6468c6300019ebb
Dese Narfa Firmansyah <jackynarfa@GMAIL.COM>
2019-04-05 22:58:06 +0700

    init SOM function

--- self_organizing_maps/__init__.py
+++ self_organizing_maps/__init__.py
@@ -16,3 +16,25 @@ def init_som_net(row_size, column_size, attribute_size):
             weight_row.append(weight_column)
         weight.append(weight_row)
     return weight
+
+
+def training(dataset_input, input_weight, epoch, alpha, eta):
+    new_weight = list()
+    return new_weight
+
+
+def penentuan_cluster(trained_weight, input_data_row):
+    return (0, 0)
+
+
+def quantization_error(trained_weight, dataset_input):
+    return 0
+
+
+def davies_bouldin_index(dataset_input, clustering_result, trained_weight):
+    return 0
+
+
+
+# ex script
+from pandas import DataFrame as df
