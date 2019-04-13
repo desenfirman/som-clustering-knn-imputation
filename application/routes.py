983e6c380e0e11f7296d973224272138969e4b1f
Dese Narfa Firmansyah <jackynarfa@GMAIL.COM>
2019-04-13 22:20:36 +0700

    add functionality to get data from POST

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from pandas import read_csv
 
 
 mod = Blueprint('main', __name__,
@@ -9,3 +10,22 @@ mod = Blueprint('main', __name__,
 @mod.route('/')
 def home():
     return render_template("main.html")
+
+
+@mod.route('/clustering', methods=['GET', 'POST'])
+def mulai_clustering():
+    data = dict()
+    if request.method == 'POST':
+        data['k_value'] = request.form['k_value']
+        data['neuron_width'] = request.form['neuron_width']
+        data['neuron_height'] = request.form['neuron_height']
+        data['alpha'] = request.form['alpha']
+        data['eta'] = request.form['eta']
+        data['epoch'] = request.form['epoch']
+        if request.form['pengukuran'] == 'qe':
+            data['tipe_pengukuran'] = 'Quantization Error'
+        elif request.form['pengukuran'] == 'dbi':
+            data['tipe_pengukuran'] = 'Davies-Bouldin Index'
+        return render_template("clustering.html", data=data)
+    else:
+        return redirect(url_for('main.home'))
