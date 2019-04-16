51cc486273feaae703f1077f0b84665a5bc8ab32
Dese Narfa Firmansyah <jackynarfa@GMAIL.COM>
2019-04-16 13:29:24 +0700

    basic cluster visualization is nearly complete

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, session, render_template, request, redirect, url_for, Response, stream_with_context
+from pprint import pprint
 import time
 from math import exp
 from pandas import read_csv
@@ -16,8 +17,13 @@ def home():
 
 def extract_dataset(file):
     dataset = read_csv(file)
-    dataset_value = dataset.iloc[:, :].values
-    return dataset_value
+    new_columns = dataset.columns.values
+    new_columns[0] = 'First_Column'
+    dataset.columns = new_columns
+    dataset = dataset.set_index('First_Column')
+    session['raw_dataset'] = dataset
+    pprint(dataset)
+    return session['raw_dataset'].iloc[:, :].values
 
 
 @mod.route('/clustering', methods=['GET', 'POST'])
@@ -47,7 +53,10 @@ def mulai_clustering():
         session['weight'] = self_organizing_maps.init_som_net(
             session['neuron_height'], session['neuron_width'], attr_size)
 
-        return render_template("clustering.html")
+        first_JSON_data = cluster_visualization_in_JSON(
+            session['weight'], session['dataset'])
+
+        return render_template("clustering.html", first_JSON_data = first_JSON_data)
     else:
         return redirect(url_for('main.home'))
 
@@ -62,8 +71,13 @@ def training_progress():
             alpha_t = session['alpha_0'] * (1 / t)
             eta_t = session['eta_0'] * exp(-1 * (t / session['max_epoch']))
 
+            cluster_visualization = cluster_visualization_in_JSON(
+                session['weight'], session['dataset'])
+
             epoch_t_response = '"epoch": ' + str(t) + ', '
             weight_response = '"weight": ' + str(session['weight']) + ', '
+            cluster_vis_t_response = '"cluster": ' + \
+                cluster_visualization + ', '
             score_t_response = '"score": ' + \
                 str(get_score(session['weight'],
                               session['dataset'])) + ', '
@@ -71,14 +85,30 @@ def training_progress():
                 str(session['tipe_pengukuran']) + '" '
 
             yield "data: {" + epoch_t_response + weight_response + \
+                cluster_vis_t_response + \
                 score_t_response + jenis_pengukuran_response + "}\n\n"
 
             session['weight'] = self_organizing_maps.one_epoch_training(
                 session['dataset'], session['weight'], alpha_t, eta_t)
             t += 1
-            time.sleep(0.5)
+            time.sleep(1.25)
 
     return Response(training(), content_type='text/event-stream')
+
+
+def cluster_visualization_in_JSON(training_weight, input_dataset):
+    indexes = list(session['raw_dataset'].index)
+    list_cluster = '['
+    for i in range(0, len(input_dataset)):
+        index = '"' + indexes[i] + '"'
+        bmu = self_organizing_maps.penentuan_cluster(
+            training_weight, input_dataset[i])
+        json = '{"id": ' + str(index) + ', "neuron": [' + \
+            str(bmu[0]) + ', ' + str(bmu[1]) + ']}'
+        list_cluster += json
+        if i != len(input_dataset) - 1:
+            list_cluster += ', '
+    return list_cluster + ']'
 
 
 def get_score(weight, dataset_input):
